60d53cfef26e678547f6a0c3aca6eb97f4227793
Eric Tomasi <etomasi2323@gmail.com>
2020-12-11 18:35:02 -0500

    feat: strip whitespace from strings in clean_address

--- fuzzyexact/clean_address.py
+++ fuzzyexact/clean_address.py
@@ -5,21 +5,21 @@ def clean_address(df, address):
     df[address] = df[address].str.upper()
 
     #abbreviate road type
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(STREET)(?!\w)', 'ST').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(ROAD)(?!\w)', 'RD').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(AVENUE)(?!\w)', 'AVE').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(CLOSE)(?!\w)', 'CL').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(COURT)(?!\w)', 'CT').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(BOULEVARDE)(?!\w)', 'BLVD').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(DRIVE)(?!\w)', 'DR').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(LANE)(?!\w)', 'LN').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(PLACE)(?!\w)', 'PL').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(SQUARE)(?!\w)', 'SQ').strip()
-    df[address] = df[address].str.replace(r'\w*(?<!\w)(CIRCUIT)(?!\w)', 'CCT').strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(STREET)(?!\w)', 'ST').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(ROAD)(?!\w)', 'RD').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(AVENUE)(?!\w)', 'AVE').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(CLOSE)(?!\w)', 'CL').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(COURT)(?!\w)', 'CT').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(BOULEVARDE)(?!\w)', 'BLVD').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(DRIVE)(?!\w)', 'DR').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(LANE)(?!\w)', 'LN').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(PLACE)(?!\w)', 'PL').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(SQUARE)(?!\w)', 'SQ').str.strip()
+    df[address] = df[address].str.replace(r'\w*(?<!\w)(CIRCUIT)(?!\w)', 'CCT').str.strip()
 
     #strip room, apt, and building numbers
-    df[address] = df[address].str.replace(r'STE[\s][\d]+$', '').strip()
-    df[address] = df[address].str.replace(r'RM[\s][\d]+$', '').strip()
-    df[address] = df[address].str.replace(r'BLDG[\s][\d]+$', '').strip()
+    df[address] = df[address].str.replace(r'STE[\s][\d]+$', '').str.strip()
+    df[address] = df[address].str.replace(r'RM[\s][\d]+$', '').str.strip()
+    df[address] = df[address].str.replace(r'BLDG[\s][\d]+$', '').str.strip()
     
     return df
