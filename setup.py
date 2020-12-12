2b419b74c6aec9613a7c040e46e6580607e9412f
Eric Tomasi <etomasi2323@gmail.com>
2020-12-11 20:20:49 -0500

    release 1.0!

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='fuzzyexact',
-      version='0.0.9',
+      version='1.0.0',
       description='Perform fuzzy matching against two pandas dataframes with optional exact matches',
       long_description=long_description,
       long_description_content_type='text/markdown',
